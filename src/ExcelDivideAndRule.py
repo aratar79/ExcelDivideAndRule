d4288d21519d4b0da74b4512b98949b3a264ca48
aratar79 <aratar79@gmail.com>
2022-06-30 10:51:20 +0200

    feat: add progressbar

--- src/ExcelDivideAndRule.py
+++ src/ExcelDivideAndRule.py
@@ -16,27 +16,62 @@ from tabulate import tabulate
 
 def exists_files(file_name):
 
+    if file_name.startswith(".\\"):
+        prefix = os.path.splitext(file_name)[0].split("\\")[1]
+    else:
+        prefix = os.path.splitext(file_name)[0]
+
+    result = 0
+    
     files = [
         filename
-        for filename in os.listdir(".")
-        if filename.startswith(f"{file_name}_output_")
+        for filename in os.listdir(os.getcwd())
+        if filename.startswith(f"{prefix}_output_")
     ]
-    names = [os.path.splitext(base)[0] for base in files]
-    end_names = [name.split("_")[-1] for name in names]
-    nums = [int(num) for num in end_names if num.isdigit()]
 
-    return max(nums)
+    if files:
+
+        names = [os.path.splitext(base)[0] for base in files]
+        if names:
+            end_names = [name.split("_")[-1] for name in names]
+            nums = [int(num) for num in end_names if num.isdigit()]
+            if nums:
+                result = max(nums) + 1
+    
+    return result
 
 
 def run_process(file_name, sheet_name, split_parts, init_row):
 
     outfile_num = exists_files(file_name)
     head_rows = init_row - 1
+    print("Loading main file, may take a few minutes....")
     main_file = pd.read_excel(file_name, sheet_name)
+    print("\n")
     main_headers = main_file.keys()
     main_num_row = (len(main_file.index) + 1) - head_rows
     headers = [head for head in main_headers]
+    columns = len(headers)
     books = []
+
+    tab_file_headers = ["File name", "Rows", "Cols", "File parts"]
+    tab_part_headers = ["Index", "File name", "Rows"]
+
+    tab_file_data = [file_name, main_num_row, columns, split_parts]
+
+    data_file = [tab_file_data]
+    data_parts = []
+    data_part = []
+
+    print(
+        tabulate(
+            data_file,
+            headers=tab_file_headers,
+            showindex="never",
+            tablefmt="simple",
+        )
+    )
+    print("\n")
 
     parts = [main_num_row // split_parts] * split_parts
     remainder = main_num_row % split_parts
@@ -47,7 +82,7 @@ def run_process(file_name, sheet_name, split_parts, init_row):
     for i in range(split_parts):
         new_file = pd.DataFrame(columns=range(len(headers)))
         new_file.columns = [head for head in headers]
-        name_file = f"{file_name}_output_{i + outfile_num}.xlsx"
+        name_file = f"{os.path.splitext(file_name)[0]}_output_{i + outfile_num}.xlsx"
         writer = pd.ExcelWriter(name_file, engine="xlsxwriter")
         books.append(name_file)
         new_file.to_excel(writer, sheet_name, index=False)
@@ -56,22 +91,32 @@ def run_process(file_name, sheet_name, split_parts, init_row):
     if not len(books) == len(parts) == split_parts:
         sys.exit()
 
-    columns = len(headers)
-
-    for i in tqdm(range(len(books)), ascii=True, desc="Split file process."):
+    for i in tqdm(range(len(books)), ascii=True, desc="Split file process.", leave=True):
         rows = parts[i]
         output_file = pd.read_excel(books[i])
+        data_part = [books[i], rows]
         values_to_add = {}
         if i == 0:
             start_row = head_rows - 1
         end_row = start_row + rows
-        for row in range(start_row, end_row, 1):
+        for row in tqdm(range(start_row, end_row, 1), ascii=True, desc=books[i], leave=False):
             for column in range(columns):
                 values_to_add[headers[column]] = main_file[headers[column]].values[row]
             row_to_add = pd.DataFrame(values_to_add, index=[row])
             output_file = pd.concat([output_file, row_to_add])
         output_file.to_excel(books[i], sheet_name, index=False)
         start_row = end_row
+        data_parts.append(data_part)
+
+    print("\n")
+    print(
+        tabulate(
+            data_parts,
+            headers=tab_part_headers,
+            showindex="always",
+            tablefmt="simple",
+        )
+    )
 
 
 def main(argv):
@@ -118,6 +163,7 @@ def main(argv):
 
 
 if __name__ == "__main__":
+    print(f'\nDirectory: {os.getcwd()}\n')
     main(
         argparse.ArgumentParser(description="Splits an excel .xlsx file into N files.")
     )
